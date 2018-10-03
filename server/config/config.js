66c6cdcc04486b30d5864aa3cef9c7ca0127acdb
Diego <dalvarez@transfiriendo.com>
2018-10-03 16:01:02 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://diegoAA:diego958668@ds251277.mlab.com:51277/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 
